0d0109b7213f09bd12873f38f779fa765da171fb
LegendaryBananaCat <joana.f.silva.99@gmail.com>
2023-11-19 14:09:31 +0000

    fixed waypoint movement; starting ExternalWaypoints

--- Uber Simulator/Assets/Assets/Jo/Scripts/Waypoins.cs
+++ Uber Simulator/Assets/Assets/Jo/Scripts/Waypoins.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class Waypoins : MonoBehaviour
 {
     public TMP_InputField playerSpeedUpdate;
 
     [SerializeField] private Transform[] routeWaypoints;
-    [SerializeField] private Transform[] availableWaypoints;
+    [SerializeField] private Transform[] externalWaypoints;
     [SerializeField] private int waypointIndex;
 
     //[SerializeField] public string playerSpeedUpdate;
@@ -30,28 +28,32 @@ public class Waypoins : MonoBehaviour
     {
         UIUpdate();
         Move();
+
+        Vector2 direction = transform.position - routeWaypoints[waypointIndex].transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        direction.Normalize();
     }
 
     private void Move()
     {
-        if(!waiting)
+        if (!waiting)
         {
             if (waypointIndex <= routeWaypoints.Length - 1)
-                    {
-                        transform.position = Vector2.MoveTowards(transform.position, routeWaypoints[waypointIndex].transform.position, playerSpeed * Time.deltaTime);
-
-                        if(transform.position == routeWaypoints[waypointIndex].transform.position)
-                        {
-                            waypointIndex += 1;
-                        }
-                    }
+            {
+                transform.position = Vector2.MoveTowards(transform.position, routeWaypoints[waypointIndex].transform.position, playerSpeed * Time.deltaTime);
+                if (transform.position == routeWaypoints[waypointIndex].transform.position)
+                {
+                    waypointIndex += 1;
+                }
+            }
         }
 
         else
         {
             //Change waypoint path
         }
-        
+
     }
 
     //Unity Input field update
