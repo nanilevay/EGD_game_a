6f6343eb2724a617785aacb3d0a2a9f154ad8005
LegendaryBananaCat <joana.f.silva.99@gmail.com>
2023-11-19 16:36:23 +0000

    Testing External route points

--- Uber Simulator/Assets/Assets/Jo/Scripts/Waypoins.cs
+++ Uber Simulator/Assets/Assets/Jo/Scripts/Waypoins.cs
@@ -1,18 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Waypoins : MonoBehaviour
 {
     public TMP_InputField playerSpeedUpdate;
+    public GameObject button;
 
     [SerializeField] private Transform[] routeWaypoints;
     [SerializeField] private Transform[] externalWaypoints;
     [SerializeField] private int waypointIndex;
+    [SerializeField] private int ExternalWaypointIndex;
 
     //[SerializeField] public string playerSpeedUpdate;
     [SerializeField] public float playerSpeed;
 
-    [SerializeField] private bool waiting;
+    [SerializeField] public bool waiting;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +30,11 @@ public class Waypoins : MonoBehaviour
     void Update()
     {
         UIUpdate();
-        Move();
+
+        if(!waiting)
+        {
+            Move();
+        }
 
         Vector2 direction = transform.position - routeWaypoints[waypointIndex].transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
@@ -37,23 +44,46 @@ public class Waypoins : MonoBehaviour
 
     private void Move()
     {
-        if (!waiting)
+        if (waypointIndex <= routeWaypoints.Length - 1)
         {
-            if (waypointIndex <= routeWaypoints.Length - 1)
+            transform.position = Vector2.MoveTowards(transform.position, routeWaypoints[waypointIndex].transform.position, playerSpeed * Time.deltaTime);
+            if (transform.position == routeWaypoints[waypointIndex].transform.position)
             {
-                transform.position = Vector2.MoveTowards(transform.position, routeWaypoints[waypointIndex].transform.position, playerSpeed * Time.deltaTime);
-                if (transform.position == routeWaypoints[waypointIndex].transform.position)
-                {
-                    waypointIndex += 1;
-                }
+                waypointIndex += 1;
             }
         }
+    }
 
-        else
+    public void ChangeRoute()
+    {
+        if (ExternalWaypointIndex <= externalWaypoints.Length - 1)
         {
-            //Change waypoint path
+            Debug.Log("Change route");
+            transform.position = Vector2.MoveTowards(transform.position, externalWaypoints[ExternalWaypointIndex].transform.position, playerSpeed * Time.deltaTime);
+            if (transform.position == externalWaypoints[ExternalWaypointIndex].transform.position)
+            {
+                Debug.Log("Update");
+                ExternalWaypointIndex += 1;
+                waiting = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("End Route"))
+        {
+            Debug.Log("End");
+            waypointIndex = 1;
+            waiting = true;
         }
 
+        if (other.gameObject.CompareTag("Event"))
+        {
+            Debug.Log("Event");
+            waiting = true;
+            button.SetActive(true);
+        }
     }
 
     //Unity Input field update
